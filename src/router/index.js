8083ce59b14927ca081bf7e8dd98876b6118699a
mido <midorin6497@gmail.com>
2019-04-13 16:41:47 +0800

    該死的檔案名稱大小寫

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import AddBook from '@/pages/AddBook'
 import Login from '@/pages/login'
-import Home from '@/pages/home'
+import Home from '@/pages/Home'
 
 Vue.use(Router)
 
